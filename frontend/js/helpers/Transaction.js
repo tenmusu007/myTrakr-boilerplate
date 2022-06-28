2f954d74495d0341ee178f3a04d65328cdeecea1
Koki Sakai <likuor0528@gmail.com>
2022-06-28 09:45:09 -0700

    Add add new users function

--- frontend/js/helpers/Transaction.js
+++ frontend/js/helpers/Transaction.js
@@ -1,47 +1,45 @@
-let radiovalue = ""
-let fromname = ""
-let toname = ""
-let Username = ""
-let transaction = ""
-let tranArr = []
-let test
-let userObj
-let total = 0
+let radiovalue = '';
+let fromname = '';
+let toname = '';
+let Username = '';
+let transaction = '';
+let tranArr = [];
+let test;
+let userObj;
+let total = 0;
 $('#addtran').on('click', (e) => {
   let radiobtn = $('input[name="type"]:checked').val();
   e.preventDefault();
   radiovalue = radiobtn;
-  let txt = $("#transtxt").val();
+  let txt = $('#transtxt').val();
   e.preventDefault();
   $.ajax({
     method: 'post',
-    data: JSON.stringify(
-      {
-        newTransaction: {
-          accountId: 1,
-          balance: total,
-          username: "atsu",
-          // username: Username,
-          typeof: "deposit",
-          // typeof: radiovalue,
-          // typeof: radiovalue,
-          category: "food",
-          from: "koki",
-          to: "atsu",
-          txt: txt,
-          // amount: 100,
-          amount: Number($("#transamount").val()),
-        }
+    data: JSON.stringify({
+      newTransaction: {
+        accountId: 1,
+        balance: total,
+        username: 'atsu',
+        // username: Username,
+        typeof: 'deposit',
+        // typeof: radiovalue,
+        // typeof: radiovalue,
+        category: 'food',
+        from: 'koki',
+        to: 'atsu',
+        txt: txt,
+        // amount: 100,
+        amount: Number($('#transamount').val()),
       },
-    ),
+    }),
     url: 'http://localhost:3000/transaction',
     dataType: 'json',
-    contentType: "application/json",
+    contentType: 'application/json',
   })
     .done((data) => {
-      console.log('data post ajax', data)
-      const dataInfo = data
-      console.log("data", dataInfo);
+      console.log('data post ajax', data);
+      const dataInfo = data;
+      console.log('data', dataInfo);
       $.ajax({
         method: 'get',
         url: 'http://localhost:3000/accounts',
@@ -49,17 +47,23 @@ $('#addtran').on('click', (e) => {
       }).done((data2) => {
         console.log('"indiv Account"', data2);
         $.each(data2, (i, post) => {
-          Username = post.username
+          Username = post.username;
           console.log(Username, post.transactions);
-          console.log("GET", post);
-          console.log("latest transaction",  post.transactions[(post.transactions.length -1)]);
-          tranArr.push(post.transactions[(post.transactions.length -1)])
-          console.log("testArr",tranArr[(tranArr.length -1)].balance);
-          test = new Deposit(post.transactions, post.transactions[(post.transactions.length -1)])
+          console.log('GET', post);
+          console.log(
+            'latest transaction',
+            post.transactions[post.transactions.length - 1]
+          );
+          tranArr.push(post.transactions[post.transactions.length - 1]);
+          console.log('testArr', tranArr[tranArr.length - 1].balance);
+          test = new Deposit(
+            post.transactions,
+            post.transactions[post.transactions.length - 1]
+          );
           test.commit();
 
           console.log('Transaction 1:', test);
-        })
+        });
       });
 
       $.each(data, (i, post) => {
@@ -83,12 +87,10 @@ $('#addtran').on('click', (e) => {
     });
 });
 
-
-
-let testArry =[0]
+let testArry = [0];
 class Transaction {
   constructor(newhistory, account) {
-    console.log("newhistory", newhistory);
+    console.log('newhistory', newhistory);
     // console.log("account", account);
     this.newhistory = newhistory;
     this.account = account;
@@ -97,27 +99,30 @@ class Transaction {
     // if (this.value < 0 && this.amount > this.account.balance) return;
     // this.account.transactions.push(this.value);
     // this.account.balance += this.value;
-    console.log("account", this.account)
-    console.log("value", this.value)
-    console.log("new balance(newhistory)", this.newhistory[(this.newhistory.length -1)].balance)
-    console.log("previous balance", this.account.balance)
-    console.log("-------------");
+    console.log('account', this.account);
+    console.log('value', this.value);
+    console.log(
+      'new balance(newhistory)',
+      this.newhistory[this.newhistory.length - 1].balance
+    );
+    console.log('previous balance', this.account.balance);
+    console.log('-------------');
     // return this.newhistory[(this.newhistory.length -1)].balance = this.value + this.account.balance
     // let currentBalance = this.value + this.account.balance
-    total = this.value + this.account.balance
-    
+    total = this.value + this.account.balance;
+
     // testArry.push(currentBalance)
-    testArry.push(total)
+    testArry.push(total);
     console.log('balance:', testArry);
     // total = testArry.reduce((sum, element)=>{
     //   return sum + element;
     // },0)
     console.log(total);
-    $("#summary").html(`
+    $('#summary').html(`
     <p>Username : ${Username}</p>
     <p>Balance : ${total}</p>
-    `)
-    return 
+    `);
+    return;
     // return this.newhistory.balance = this.value + this.newaccount.balance
     // return balance
   }
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -2,38 +2,113 @@ $(() => {
   //Start coding here!
   $('[name=from]').change(() => {
     let name = $('[name=from] option:selected').text();
-    fromname = name
+    fromname = name;
     console.log(name);
   });
   $('[name=to]').change(() => {
     let name = $('[name=to] option:selected').text();
-    toname = name
+    toname = name;
     console.log(name);
   });
   $('[name=type]').change((e) => {
-  let radiobtn = $('input[name="type"]:checked').val();
-  e.preventDefault();
-  radiovalue = radiobtn;
-  console.log(radiovalue);
-  if(radiovalue === "withdraw" || radiovalue === "deposit" ){
-    $("#transfer").hide();
-    $("#account").show()
-  }else if(radiovalue === "transfer"){
-    $("#account").hide()
-    $("#transfer").show();
-  }
-  })
-
-// categroy
-// $("#categorybox").hide()
-$("#addcategroy").on("click",(e)=>{
-  e.preventDefault();
-  $("#categorybox").hide()
-})
-// $('[name=category]').change(() => {
-//   let test = $('[name=category] option:selected').text();
-//   fromname = name
-//   console.log(name);
-// });
+    let radiobtn = $('input[name=“type”]:checked').val();
+    e.preventDefault();
+    radiovalue = radiobtn;
+    console.log(radiovalue);
+    if (radiovalue === 'withdraw' || radiovalue === 'deposit') {
+      $('#transfer').hide();
+      $('#account').show();
+    } else if (radiovalue === 'transfer') {
+      $('#account').hide();
+      $('#transfer').show();
+    }
+  });
+  // categroy
+  // $(“#categorybox”).hide()
+  $('#addcategroy').on('click', (e) => {
+    e.preventDefault();
+    $('#categorybox').hide();
+  });
+  // $('[name=category]').change(() => {
+  //   let test = $('[name=category] option:selected').text();
+  //   fromname = name
+  //   console.log(name);
+  // });
+  // Koki part /////////////////////////////////////
+  users = [];
+  $.ajax({
+    method: 'get',
+    url: 'http://localhost:3000/accounts',
+    dataType: 'json',
+  }).done((data) => {
+    //add new users
+    $('#btnAddAccount').click(function (event) {
+      let inputVal = $('#newUserName').val();
+      jsonData = JSON.stringify({
+        newAccount: {
+          username: inputVal,
+          transactions: [],
+        },
+      });
+      if (inputVal !== '' && data.length === 0) {
+        //first add
+        $.ajax({
+          method: 'post',
+          url: 'http://localhost:3000/accounts',
+          dataType: 'json',
+          data: jsonData,
+          contentType: 'application/json',
+        })
+          .done((data) => {
+            console.log('first success', data);
+            users.push(new Account(data.username, data.transactions));
+            $('#selectUser').append(
+              $('<option>').html(data.username).val(data.username)
+            );
+          })
+          .fail((error) => {
+            console.log('error', error);
+          });
+      } else if (inputVal !== '' && data.length !== 0) {
+        $.ajax({
+          method: 'post',
+          url: 'http://localhost:3000/accounts',
+          dataType: 'json',
+          data: jsonData,
+          contentType: 'application/json',
+        })
+          .done((data) => {
+            console.log('success', data);
+            users.push(new Account(data.username, data.transactions));
+            $('#selectUser').append(
+              $('<option>').html(data.username).val(data.username)
+            );
+          })
+          .fail((error) => {
+            console.log('error', error);
+          });
+      } else {
+        message = 'Error username is invalid';
+        console.log(message);
+      }
+      event.preventDefault();
+    });
+    // console.log(users);
+    const setUser = () => {
+      $('[name=username]').change(() => {
+        let selectedUser = $('[name=username]').val();
+        for (let index = 0; index < users.length; index++) {
+          if (users[index].username === selectedUser) {
+            console.log(users[index]);
+            $('#summary').html(`
+              <p>Username : ${users[index].username}</p>
+              <p>Balance : ${users[index].balance}</p>
+            `);
+            return users[index];
+          }
+        }
+      });
+    };
+    setUser();
+  });
 });
-
