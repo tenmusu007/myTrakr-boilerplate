fedb3341e8eb1a70bc1ef1de13d4fae8364a2615
Atsuya Tanaka <tenmusu007@gmail.com>
2022-07-04 18:26:22 -0700

    fixed transfer section

--- frontend/js/helpers/Transfer.js
+++ frontend/js/helpers/Transfer.js
@@ -2,4 +2,8 @@ class Transfer extends Transaction {
     constructor(amount, account){
         super(account.accountIdFrom, account.accountIdTo)
     }
+    // test(){
+    //     super.test()
+    //     console.log(this.account.accountIdFrom, this.account.accountIdTo);
+    // }
 }
